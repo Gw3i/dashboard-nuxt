59b0e7769b6674cf8d9af3c4e2a2c9e9246a9a08
Vlady <contact.johngoldco@gmail.com>
2022-09-20 11:58:05 +0200

    Position elements

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="d-flex">
       <Nav />
-      <header>
+      <header class="d-flex p-2 w-100 h-25 justify-content-between">
         <h1>Dashboard</h1>
         <button @click="hello">Create Job</button>
       </header>
