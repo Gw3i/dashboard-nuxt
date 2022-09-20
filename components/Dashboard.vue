9cbad9bdcb791fc8036950be1e8b0e89ecafad08
Vlady <contact.johngoldco@gmail.com>
2022-09-20 10:12:51 +0200

    Add Dashboard component

---
+++ components/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header>
+      <h1>Dashboard</h1>
+      <button @click="hello">Create Job</button>
+    </header>
+    <main></main>
+  </div>
+</template>
+
+<script setup>
+function hello() {
+  alert('Hello!')
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <Tutorial />
+  <div>
+    <Dashboard />
+  </div>
 </template>
 
 <script>
+import Dashboard from '~/components/Dashboard.vue'
 export default {
   name: 'IndexPage',
+  components: { Dashboard },
 }
 </script>
